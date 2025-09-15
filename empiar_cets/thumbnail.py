5c6cd11346d3db9a2a0d5e6cde8de3147c06e4b6
Chr1st0p43rR <190404576+Chr1st0p43rR@users.noreply.github.com>
2025-09-15 17:07:03 +0100

    Refactored thumbnail functions for clarity, and a more robust projection size calculation.

--- empiar_cets/thumbnail.py
+++ empiar_cets/thumbnail.py
@@ -70,36 +70,39 @@ def project_and_scale_coordinates(
     return scaled_coords
 
 
-def create_tomogram_thumbnail(
+def make_tomogram_projection(
         tomogram_data: np.ndarray, 
-        thumbnail_size: tuple[int, int],
-        coordinates: Union[list[tuple[float, float]], None],
-        method: str, 
+        projection_method: str, 
         limit_projection: float, 
-):
+) -> np.ndarray:
     
     depth = tomogram_data.shape[0]
     if limit_projection < 1.0:
-        central_slice = depth // 2
-        half_window = int((depth * limit_projection) // 2)
-        start_slice = max(0, central_slice - half_window)
-        end_slice = min(depth, central_slice + half_window)
+        half_window = (depth * limit_projection) / 2
+        stack_centre = (depth -1 ) / 2
+        start_slice = round(max(0, stack_centre - half_window))
+        end_slice = round(min(depth, stack_centre + half_window))
         tomogram_data = tomogram_data[start_slice:end_slice, :, :]
         logger.info(f"Projecting over slices {start_slice} to {end_slice} (total {end_slice - start_slice})")
 
-    if method == "max":
+    if projection_method == "max":
         projection = np.max(tomogram_data, axis=0)
-    elif method == "mean":
+    elif projection_method == "mean":
         projection = np.mean(tomogram_data, axis=0)
-    elif method == "middle":
+    elif projection_method == "middle":
         middle_idx = tomogram_data.shape[0] // 2
         projection = tomogram_data[middle_idx]
     else:
         raise ValueError("Method must be 'max', 'mean', or 'middle'")
+
+    return projection
+
+
+def convert_projection_to_rgb_thumbnail(
+        projection: np.ndarray, 
+        thumbnail_size: tuple[int, int], 
+) -> Image.Image:
     
-    # TODO: some check of orientation?
-    # TODO: and dimensions?
-    projection = np.flipud(projection) 
     projection = projection.astype(np.float64)
     projection = (projection - projection.min()) / (projection.max() - projection.min())
     projection = (projection * 255).astype(np.uint8)
@@ -107,23 +110,63 @@ def create_tomogram_thumbnail(
     img = Image.fromarray(projection, mode='L')
     img.thumbnail(thumbnail_size, Image.Resampling.LANCZOS)
     img = img.convert('RGB')
-    
-    if coordinates:
-
-        draw = ImageDraw.Draw(img)
-        point_size = 3
-        point_color = (0, 255, 255, 64) 
-        for x, y in coordinates:
-            left = x - point_size
-            top = y - point_size
-            right = x + point_size
-            bottom = y + point_size
-            
-            draw.ellipse([left, top, right, bottom], 
-                        fill=None, 
-                        outline=point_color)
 
     return img
+
+
+def plot_annotation_points_on_image(
+        thumbnail_img: Image.Image, 
+        coordinates: list[tuple[float, float]],
+) -> Image.Image:
+    
+    draw = ImageDraw.Draw(thumbnail_img)
+    point_size = 3
+    point_color = (0, 255, 255, 64) 
+    for x, y in coordinates:
+        left = x - point_size
+        top = y - point_size
+        right = x + point_size
+        bottom = y + point_size
+        
+        draw.ellipse([left, top, right, bottom], 
+                    fill=None, 
+                    outline=point_color)
+    
+    return thumbnail_img
+
+
+def create_tomogram_thumbnail(
+        tomogram_data: np.ndarray, 
+        thumbnail_size: tuple[int, int],
+        coordinates: Union[list[tuple[float, float]], None],
+        projection_method: str, 
+        limit_projection: float, 
+):
+    
+    projection = make_tomogram_projection(
+        tomogram_data, 
+        projection_method, 
+        limit_projection, 
+    )
+    
+    # TODO: some check of orientation?
+    # TODO: and dimensions?
+    # TODO: establish convention for orientation of axes
+    # in cases seen so, projection requires flipping vertically
+    projection = np.flipud(projection) 
+
+    thumbnail_img = convert_projection_to_rgb_thumbnail(
+        projection, 
+        thumbnail_size, 
+    )
+    
+    if coordinates:
+        thumbnail_img = plot_annotation_points_on_image(
+            thumbnail_img, 
+            coordinates
+        )
+        
+    return thumbnail_img
 
 
 def process_tomogram_thumbnail(
@@ -156,34 +199,33 @@ def process_tomogram_thumbnail(
             download_mrc_file(tomo_path, cache_filepath)
 
         with mrcfile.open(cache_filepath, mode="r") as mrc:
-
-            # TODO: allow for more than one annotation
-            # TODO: allow for other annotations types (assume point curently)
-            # TODO: make a field for correspondence of annotation to tomogram
-
             tomo_data = mrc.data.copy()
+        
+        # TODO: allow for more than one annotation
+        # TODO: allow for other annotations types (assume point curently)
+        # TODO: make a field for correspondence of annotation to tomogram
 
-            projected_coords = None
-            if annotations is not None:
-                if len(annotations) == 1:
-                    json_file_path = annotations[0]["path"]
-                    original_coords = load_star_coordinates_from_json(json_file_path)
-                    projected_coords = project_and_scale_coordinates(
-                        original_coords, 
-                        tomo_data.shape, 
-                        thumbnail_size, 
-                        limit_annotation, 
-                    )
-                else:
-                    logger.info("More than one annotation set found; skipping.")
+        projected_coords = None
+        if annotations is not None:
+            if len(annotations) == 1:
+                json_file_path = annotations[0]["path"]
+                original_coords = load_star_coordinates_from_json(json_file_path)
+                projected_coords = project_and_scale_coordinates(
+                    original_coords, 
+                    tomo_data.shape, 
+                    thumbnail_size, 
+                    limit_annotation, 
+                )
+            else:
+                logger.info("More than one annotation set found; skipping.")
 
-            thumbnail = create_tomogram_thumbnail(
-                tomo_data, 
-                thumbnail_size, 
-                projected_coords, 
-                projection_method, 
-                limit_projection, 
-            )
-            
-            thumbnail_path = os.path.join(output_dir, f"{filename}_{projection_method}_thumbnail.png")
-            thumbnail.save(thumbnail_path)
+        thumbnail = create_tomogram_thumbnail(
+            tomo_data, 
+            thumbnail_size, 
+            projected_coords, 
+            projection_method, 
+            limit_projection, 
+        )
+        
+        thumbnail_path = os.path.join(output_dir, f"{filename}_{projection_method}_thumbnail.png")
+        thumbnail.save(thumbnail_path)
